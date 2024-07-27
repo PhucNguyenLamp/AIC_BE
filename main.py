bc72bab67551f582b6a256f56973c52831a5ad3f
Beginner <quantran2381@gmail.com>
2024-07-27 22:24:16 +0300

    feat: add middleware

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import List
+from fastapi import FastAPI, Request
+from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from app.common.exceptions.base import CustomException
 from app.routers.v1 import v1_router
 from app.config.config import settings
 from app.models.text import Text
 from beanie import init_beanie
 from contextlib import asynccontextmanager
 from motor.motor_asyncio import AsyncIOMotorClient
+from app.common.middlewares import ResponseLoggerMiddleware
+
+
+def on_auth_error(request: Request, exc: Exception):
+    status_code, error_code, message = 401, None, str(exc)
+    if isinstance(exc, CustomException):
+        status_code = int(exc.code)
+        error_code = exc.error_code
+        message = exc.message
+
+    return JSONResponse(
+        status_code=status_code,
+        content={"error_code": error_code, "message": message},
+    )
+
+
+def init_listeners(app_: FastAPI) -> None:
+    @app_.exception_handler(CustomException)
+    async def custom_exception_handler(request: Request, exc: CustomException):
+        return JSONResponse(
+            status_code=exc.code,
+            content={"error_code": exc.error_code, "message": exc.message},
+        )
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-
     print("Starting application", settings)
     # Setup mongoDB
     app.client = AsyncIOMotorClient(
@@ -24,11 +50,31 @@ async def lifespan(app: FastAPI):
     yield
 
 
+def make_middleware() -> List[Middleware]:
+    middleware = [
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        ),
+        # Middleware(
+        #     on_error=on_auth_error,
+        # ),
+        Middleware(ResponseLoggerMiddleware),
+    ]
+    return middleware
+
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     lifespan=lifespan,
+    middleware=make_middleware(),
 )
+
+init_listeners(app_=app)
 
 app.add_middleware(
     CORSMiddleware,
